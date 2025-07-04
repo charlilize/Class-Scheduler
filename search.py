2cce986143990174716dd02b33621e1d8e66dc1d
Charlize <148724296+charlilize@users.noreply.github.com>
2025-07-03 21:14:29 -0700

    Feat: UI for entering multiple classes

--- search.py
+++ search.py
@@ -6,36 +6,39 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
 
+
 # Gather the class info before searching
-subject = input("Enter the subject abbreviation you wish to find: ")
+# courses = input("Enter the courses you wish to enroll in comma-seperated (e.g. CS 219, GEOL 102)\n")
+# courses = [ course.strip() for course in courses.split(",")]                                                      # Remove commas/whitespace
 
-service = Service(executable_path="./chromedriver")
-driver = webdriver.Chrome(service=service)
+# service = Service(executable_path="./chromedriver")
+# driver = webdriver.Chrome(service=service)
 
-driver.get("https://my.unlv.nevada.edu/psp/lvporprd_10/EMPLOYEE/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL")
+# driver.get("https://my.unlv.nevada.edu/psp/lvporprd_10/EMPLOYEE/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL")
 
-WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-  (By.TAG_NAME, "iframe")
-))
+# maybe underneath as a for loop for each course
+# WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+#   (By.TAG_NAME, "iframe")
+# ))
 
-# Switch to the iframe the input fields are in
-iframes = driver.find_elements(By.TAG_NAME, "iframe")
-driver.switch_to.frame(iframes[0])
+# # Switch to the iframe the input fields are in
+# iframes = driver.find_elements(By.TAG_NAME, "iframe")
+# driver.switch_to.frame(iframes[0])
 
-# ENTER THE CLASS 
-# Wait for 5 seconds for the element to exist before crashing program
-WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-  (By.ID, "SSR_CLSRCH_WRK_SUBJECT$0")
-))
+# # ENTER THE CLASS 
+# # Wait for 5 seconds for the element to exist before crashing program
+# WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+#   (By.ID, "SSR_CLSRCH_WRK_SUBJECT$0")
+# ))
 
-if "SSR_CLSRCH_WRK_SUBJECT$0" in driver.page_source:
-  print("✅ Entering class name...")
+# if "SSR_CLSRCH_WRK_SUBJECT$0" in driver.page_source:
+#   print("✅ Entering class name...")
 
-  # Get the class input field and clear it
-  input_class_element = driver.find_element(By.ID, "SSR_CLSRCH_WRK_SUBJECT$0")
-  input_class_element.clear()
+#   # Get the class input field and clear it
+#   input_class_element = driver.find_element(By.ID, "SSR_CLSRCH_WRK_SUBJECT$0")
+#   input_class_element.clear()
 
-  # Enter class slowly
-  for char in subject:
-      input_class_element.send_keys(char)
-      time.sleep(0.5)
+#   # Enter class slowly
+#   for char in subject:
+#       input_class_element.send_keys(char)
+#       time.sleep(0.5)
